2cde4b9cd63a772823c5746455d85dfaf06dbbee
root <root@3878037d169f>
2020-04-14 22:00:46 +0000

    I learned how to make a robot draw shapes for me

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,25 +1,35 @@
 package section2;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 2. Create a new Robot
-
+	Robot ironGiant=new Robot();
+	
+	
+	
 	void go() {
-		// 4. Make the robot move as fast as possible
-
-		// 5. Set the pen width to 5
-
-		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
-			// 7. Set the pen color to random
+		ironGiant.setSpeed(100);
+		
+		ironGiant.penDown();
+		
 	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
 
+		ironGiant.setPenWidth(5);
+
+		for (int i = 0; i < 4; i++) {
+
+			ironGiant.setPenColor(Color.blue);
+	
+			drawSquare();
+	
+			ironGiant.turn(90);
+			
+			
+		}
 	}
 
 	
@@ -27,7 +37,10 @@ public class FourSquare {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
 		
-		
+		for (int i=0; i < 4; i++) {
+			ironGiant.move(200);
+			ironGiant.turn(90);
+		}
 	}
 
 	public static void main(String[] args) {
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -9,25 +9,42 @@ import org.jointheleague.graphical.robot.Robot;
 public class PentagonCrazy {
 
 	private void makePrettyThings() {
-		// 1. Create a new Robot
+		Robot ironGiant=new Robot();
+	
+		
+		ironGiant.move(200);
+		
+		ironGiant.penDown();
+		
+		for (int i = 0; i < 200; i++) {
+			
+		}
+		int sides=90;
+		
+		int angles=360/sides;
+		
+		ironGiant.turn(angles);
+		
+		
+		
 
-		// 3. Put the robot's pen down
+		
 
 		// 8. Make the robot go at maximum speed (100)
 
 		// 9. Set the pen to a color that you like for the shape
 
-		// 4. Make a variable for the number of sides you want (can’t test this one)
+	
 
-		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
+		
 
 		// 7. Use a for loop to repeat the steps below (#2, #10, #6, #11) 200 times. When this is done you should see a pentagon
 
-			// 2. Move the robot 200 pixels
+		
 	
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
 	
-			// 6. Turn the robot the amount in your angle variable
+			
 	
 			// 11. Turn the robot one more degree
 
